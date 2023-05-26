43dc04022d7d0839f1086859154e00788a7efe50
Nikita.Prajapati <nikita.p@simformsolutions.com>
2023-05-26 16:24:06 +0530

    workflow config

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,16 @@ const bodyParser=require('body-parser');
 const cookieParser=require('cookie-parser');
 const api=require('./routes/api');
 const cors=require('cors');
-
+const path=require('path');
 const app = express();
 app.use(cors({
     origin:'http://localhost:5173'
 }));
 app.use(express.json()); //which will parse any incoming JSON from the body of any incoming request
-app.use('/v1',api);
+app.use(express.static(path.join(__dirname,'..','public')))
 
+app.use('/v1',api);
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
+});
 module.exports = app;
