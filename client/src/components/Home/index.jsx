7aecda38c87037619f04798ab159575c350ef079
nikita.p <nikita.p@simformsolutions.com>
2024-08-27 14:22:33 +0530

    test CI/CD

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import { Image } from "antd";
 import hospitalImage from "../../assets/hospital.webp";
-import Header from "../Header";
 import Footer from "../Footer";
+import Header from "../Header";
 
 function Home() {
   return (
@@ -10,9 +9,9 @@ function Home() {
       <Header />
       <div className="container">
         <Image src={hospitalImage} height={700} width={1900} />
-        <p>Welcome to hospital management portal</p>
+        <p>Welcome to hospital management portal test CI/CD</p>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
